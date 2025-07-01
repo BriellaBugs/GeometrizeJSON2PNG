1c06c4c9339b52c97114548d2a8bda9426478674
But you didnt have to cut me off <72235413+BriellaBugs@users.noreply.github.com>
2025-07-01 10:49:47 -0300

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
 	t.identity = "Geometrize JSON Renderer"
 	t.version = "11.5"
-	t.window.title = "Geometrize JSON Renderer"
+	t.window.title = "Geometrize JSON Renderer v1.2.1"
 	t.window.icon = "icon.png"
 	t.window.width = 450
 	t.window.height = 100
